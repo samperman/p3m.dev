708800d169cd7f1e25190406d0b1d552377c1b17
Sam Perman <sam@permans.com>
2025-04-28 11:52:42 -0400

    make it obvious

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("p3m.dev"),
+    titlePanel("packagemanager.posit.co"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
